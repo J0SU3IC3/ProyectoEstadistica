3fbbbf412a9f890a432009db5a8285925bf9938e
J0SU3IC3 <zS17013400@estudiantes.uv.com>
2021-10-20 13:16:12 -0500

    Se creo la función "taxicab, que sera de utilidad mas adelante en la creacion de los datos sinteticos"

--- metricas.R
+++ metricas.R
@@ -1,2 +1,14 @@
-print(1,2)
-print(2,0)
+taxicab <- function(x,y){
+  if ((length(x)-length(y))==0){
+    absolutes <- rep(0,length(x))
+    for (i in 1:length(x)) {
+      absolutes[i] <- abs(x[i]-y[i])
+    }
+    return(sum(absolutes))
+  }
+  else{
+    stop("La dimension de los vectores no es la misma")
+  }
+}
+
+
